b81db76c40f0c62fb9100b6b331060641b1fb3f7
Nosnos-ke <jin.furusho@gmail.com>
2026-09-21 23:50:53 +0900

    Botton_UnComplete

---
+++ Assets/Class1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class QuitManager : MonoBehaviour
+{
+    // ボタンが押されたときに呼ばれるメソッド
+    public void QuitApp()
+    {
+#if UNITY_EDITOR
+        // Unityエディタのプレイモードを終了する
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // ビルドされたアプリケーションを終了する
+        Application.Quit();
+#endif
+    }
+}
